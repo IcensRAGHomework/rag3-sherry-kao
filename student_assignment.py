d9aba61ea5117174fc12054fe5b44c15d7c5bffa
sherry-kao <sherry_kao@asus.com>
2025-03-05 14:16:57 +0800

    hw3-2

--- student_assignment.py
+++ student_assignment.py
@@ -70,19 +70,7 @@ def generate_hw01():
 
     
 def generate_hw02(question, city, store_type, start_date, end_date):
-    chroma_client = chromadb.PersistentClient(path=dbpath)
-    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-        api_key = gpt_emb_config['api_key'],
-        api_base = gpt_emb_config['api_base'],
-        api_type = gpt_emb_config['openai_type'],
-        api_version = gpt_emb_config['api_version'],
-        deployment_id = gpt_emb_config['deployment_name']
-    )
-    collection = chroma_client.get_or_create_collection(
-        name="TRAVEL",
-        metadata={"hnsw:space": "cosine"},
-        embedding_function=openai_ef
-    )
+    collection = generate_hw01()
     
     query_text = question
     query_city = city
